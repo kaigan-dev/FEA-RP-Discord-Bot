85ec467e896a43c331166ccaad07ee94a9fb8c1f
kaigan-dev <adbolan@pm.me>
2026-05-13 07:15:40 -0500

    debug: Additional debug messaging.

--- swg_chat_bridge.py
+++ swg_chat_bridge.py
@@ -800,6 +800,7 @@ class ChatBridge(discord.Client):
             text = msg_arr[1]
             mood_name = msg_arr[2]
             language_id = msg_arr[3]
+            self.log.info(f"{command}, {text}, {mood_name}, {language_id}")
             if command == "DM":
                 asyncio.ensure_future(self._send_to_discord(self.chat_channel, f"**{player}:** ***{text}***"))
             elif command == "emote":
