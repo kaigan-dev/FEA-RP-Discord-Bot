9512922b7bc6ae95bf72d5395bbdf5057f2dbc44
kaigan-dev <adbolan@pm.me>
2026-05-12 19:50:28 -0500

    fix: Adjust mood syntax.

--- swg_chat_bridge.py
+++ swg_chat_bridge.py
@@ -879,69 +879,69 @@ class ChatBridge(discord.Client):
             case " ":
                 return ""
             case "angry":
-                return "angrily"
+                return " angrily"
             case "bubbly":
-                return "bubbily"
+                return " bubbily"
             case "bloodthirsty":
-                return "bloodthirstily"
+                return " bloodthirstily"
             case "crotchety":
-                return "crotchetily"
+                return " crotchetily"
             case "cocky":
-                return "cockily"
+                return " cockily"
             case "courtly":
-                return "courtily"
+                return " courtily"
             case "dainty":
-                return "daintily"
+                return " daintily"
             case "dreamy":
-                return "dreamily"
+                return " dreamily"
             case "drunk":
-                return "drunkenly"
+                return " drunkenly"
             case "emphatic":
-                return "emphatically"
+                return " emphatically"
             case "evil":
-                return "evily"
+                return " evily"
             case "friendly":
-                return "friendlily"
+                return " friendlily"
             case "forgive":
-                return "forgivingly"
+                return " forgivingly"
             case "gloomy":
-                return "gloomily"
+                return " gloomily"
             case "grumpy":
-                return "grumpily"
+                return " grumpily"
             case "goofy":
-                return "goofily"
+                return " goofily"
             case "guilty":
-                return "guiltily"
+                return " guiltily"
             case "happy":
-                return "happily"
+                return " happily"
             case "haughty":
-                return "haughtily"
+                return " haughtily"
             case "hungry":
-                return "hungrily"
+                return " hungrily"
             case "lazy":
-                return "lazily"
+                return " lazily"
             case "none":
                 return ""
             case "lofty":
-                return "loftily"
+                return " loftily"
             case "shifty":
-                return "shiftily"
+                return " shiftily"
             case "silly":
-                return "sillily"
+                return " sillily"
             case "sleepy":
-                return "sleepily"
+                return " sleepily"
             case "surly":
-                return "surlily"
+                return " surlily"
             case "snobby":
-                return "snobbily"
+                return " snobbily"
             case "sorry":
-                return "sorrily"
+                return " sorrily"
             case "thirsty":
-                return "thirstily"
+                return " thirstily"
             case "wary":
-                return "warily"
+                return " warily"
             case "whiny":
-                return "whinily"
+                return " whinily"
             case _: return mood_name+"ly"
 
 # =============================================================================
